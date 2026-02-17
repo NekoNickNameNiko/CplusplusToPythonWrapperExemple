e837da574f9fb008353f613550b9a554bc0ea2d7
TANG Victor <131145275+NekoNickNameNiko@users.noreply.github.com>
2026-02-17 11:31:10 +0100

    [Update] Prebuild commands

--- OpenGL_Modern/OpenGL_Modern.cpp
+++ OpenGL_Modern/OpenGL_Modern.cpp
@@ -19,7 +19,7 @@
 int main(void)
 {
 	// Python Init
-	Bwt::PyUtility::AddNewPath("Code/python"); // add default path for python script
+	Bwt::PyUtility::AddNewPath("Code/python/PyScripts"); // add default path for python script
 
 	{
 		Bwt::Core::Application app{ 1920, 1080, "Scene Modern OpenGL" };   
