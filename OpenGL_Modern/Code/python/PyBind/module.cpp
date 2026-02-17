7c6c3a56984d7c46cec8b0185d6a180d403f9fef
TANG Victor <131145275+NekoNickNameNiko@users.noreply.github.com>
2026-02-17 10:28:09 +0100

    [Update] Module in Embeded

--- OpenGL_Modern/Code/python/PyBind/module.cpp
+++ OpenGL_Modern/Code/python/PyBind/module.cpp
@@ -1,11 +1,11 @@
-#include <pybind11/pybind11.h>
+#include <pybind11/embed.h>
 #include "bind_vector3.cpp"
 #include "bind_camera.cpp"
 
 void bind_Vector3(pybind11::module_& m);
 void bind_Camera(pybind11::module_& m);
 
-PYBIND11_MODULE(engine, m)
+PYBIND11_EMBEDDED_MODULE(engine, m)
 {
     bind_Vector3(m);
     bind_Camera(m);
